28ca5ba452ed7e01bbaad986d6a4c3c4056e9667
lolcatproductions <66800621+lolcatproductions@users.noreply.github.com>
2022-11-18 20:25:40 +0100

    finish card_feeder
    finish implementation of the card_feeder method and made practice repeat
    until all cards are learned

--- card_feeder.py
+++ card_feeder.py
@@ -1,13 +1,12 @@
-def simple(cards: list, settings: dict) -> list:
+def shuffle_simple(cards: list, settings: dict) -> list:
     """returns list of card indexes to practice (simple algorithm)"""
     from random import shuffle
 
     card_order = []
     for i, card in enumerate(cards):
-        print(i)
-        if card["competence"] == settings["max_competence"]:
-            cards.pop(i)
-        else:
+        print(i, end=" ")  # DEBUG
+        if card["competence"] < settings["max_competence"]:
             card_order.append(i)
     shuffle(card_order)
-    return card_order
+    print("")  # print newline after printing card numbers
+    return card_order  # DEBUG
--- main_cli.py
+++ main_cli.py
@@ -11,17 +11,21 @@ def practice():
         cards_raw = f_content["cards"]
         settings = f_content["settings"]
 
-    cards = card_feeder.simple(cards_raw, settings)
-    for flashcard in cards:
-        user_answer = input(f"{flashcard.get('q')}: ")
-        feedback_list, correct = feedback.check_answer(user_answer, flashcard)
-        if correct:
-            print("Correct!\n")
-        else:
-            print(f"Incorrect!\nYour answer:\n{feedback_list[0]}\n{feedback_list[1]}\nCorrect answer:\n{feedback_list[2]}\n{feedback_list[3]}")
+    while True:
+        cards = card_feeder.shuffle_simple(cards_raw, settings)
+        if len(cards = 0):
+            print("Practice finished!")
+            break
+        for flashcard_index in cards:
+            user_answer = input(f"{cards_raw[flashcard_index].get('q')}: ")
+            feedback_list, correct = feedback.check_answer(user_answer, cards_raw[flashcard_index])
+            if correct:
+                print("Correct!\n")
+            else:
+                print(f"Incorrect!\nYour answer:\n{feedback_list[0]}\n{feedback_list[1]}\nCorrect answer:\n{feedback_list[2]}\n{feedback_list[3]}")
 
-        flashcard = feedback.update_rating(flashcard, correct, settings["max_competence"])
-        set_edit.update_set(set_file, cards_raw)
+            cards_raw[flashcard_index] = feedback.update_rating(cards_raw[flashcard_index], correct, settings["max_competence"])
+            set_edit.update_set(set_file, cards_raw)
 
 
 def import_csv():
